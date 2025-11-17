b9271faf807bc27c61fe385b53314b1e952e8b0b
Bolin Lai <hlai9969@berkeley.edu>
2025-11-16 18:32:56 -0800

    project5 draw rem finish, tile pixel 16*16

--- proj5/src/core/test.java
+++ proj5/src/core/test.java
@@ -1,124 +1,43 @@
 package core;
 
-import tileengine.*;
-import edu.princeton.cs.algs4.StdDraw;
+import tileengine.TERenderer;
+import tileengine.TETile;
+import tileengine.Tileset;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
 
 /**
- * Fully self-contained program:
- * - loads image
- * - slices into tiles
- * - creates TETiles for each tile
- * - renders inside TERenderer
+ *  Draws a world that is mostly empty except for a small region.
  */
-public class ShowImageInTiles {
+public class test {
 
-    private static final int WORLD_WIDTH = 80;
-    private static final int WORLD_HEIGHT = 50;
+    private static final int WIDTH = 20;
+    private static final int HEIGHT = 56;
 
-    private static final int TILE_SIZE = 32;  // Or 16 or 64
-    private static final String INPUT_IMAGE = "resources/rem.png";
-    private static final String OUTPUT_FOLDER = "resources/remtiles";
-
-    public static void main(String[] args) throws Exception {
-
-        // 1. Split image into tiles
-        int[] dims = splitImage(INPUT_IMAGE, TILE_SIZE, OUTPUT_FOLDER);
-        int tilesX = dims[0];
-        int tilesY = dims[1];
-
-        // 2. Load tiles as TETile objects
-        TETile[][] remTiles = loadTiles(OUTPUT_FOLDER, tilesX, tilesY);
-
-        // 3. Build empty world
-        TETile[][] world = new TETile[WORLD_WIDTH][WORLD_HEIGHT];
-        for (int x = 0; x < WORLD_WIDTH; x++) {
-            for (int y = 0; y < WORLD_HEIGHT; y++) {
-                world[x][y] = Tileset.NOTHING;
-            }
-        }
-
-        // 4. Place the mosaic into the world
-        int startX = 10;
-        int startY = 10;
-        for (int x = 0; x < tilesX; x++) {
-            for (int y = 0; y < tilesY; y++) {
-                world[startX + x][startY + y] = remTiles[x][y];
-            }
-        }
-
-        // 5. Render using TERenderer
+    public static void main(String[] args) {
+        // initialize the tile rendering engine with a window of size WIDTH x HEIGHT
         TERenderer ter = new TERenderer();
-        ter.initialize(WORLD_WIDTH, WORLD_HEIGHT);
+        ter.initialize(WIDTH, HEIGHT);
+
+        // initialize tiles
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        String prefix = "C:\\Users\\thest\\Downloads\\rem small pix";  // change this file path
+        //"C:\Users\thest\Downloads\rem small pix\tile_10_20.png"
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                world[x][y] = new TETile(
+                        ' ',
+                        Color.WHITE,
+                        Color.BLACK,
+                        "img " + x + "," + y,
+                        prefix + "\\tile_" + x + "_" + (HEIGHT - 1 - y) +".png", 0
+                );
+            }
+        }
+
+        // draws the world to the screen
         ter.renderFrame(world);
     }
 
-    // ---------------------------------------------------------
-    // IMAGE SPLITTING FUNCTION
-    // ---------------------------------------------------------
 
-    /**
-     * Splits an image into TILE_SIZE × TILE_SIZE squares.
-     * Saves them as tile_x_y.png.
-     * Returns {tilesX, tilesY}.
-     */
-    public static int[] splitImage(String path, int tileSize, String outputFolder) throws Exception {
-        BufferedImage img = ImageIO.read(new File(path));
-
-        int width = img.getWidth();
-        int height = img.getHeight();
-
-        int tilesX = width / tileSize;
-        int tilesY = height / tileSize;
-
-        new File(outputFolder).mkdirs();
-
-        for (int x = 0; x < tilesX; x++) {
-            for (int y = 0; y < tilesY; y++) {
-                BufferedImage tile = img.getSubimage(
-                        x * tileSize,
-                        y * tileSize,
-                        tileSize,
-                        tileSize
-                );
-
-                File out = new File(outputFolder + "/tile_" + x + "_" + y + ".png");
-                ImageIO.write(tile, "png", out);
-            }
-        }
-
-        System.out.println("Split into " + tilesX + " × " + tilesY + " tiles.");
-        return new int[]{tilesX, tilesY};
-    }
-
-    // ---------------------------------------------------------
-    // TILE LOADING FUNCTION
-    // ---------------------------------------------------------
-
-    /**
-     * Loads tile images from folder into TETile[][].
-     */
-    public static TETile[][] loadTiles(String folder, int tilesX, int tilesY) {
-        TETile[][] tiles = new TETile[tilesX][tilesY];
-
-        for (int x = 0; x < tilesX; x++) {
-            for (int y = 0; y < tilesY; y++) {
-                String file = folder + "/tile_" + x + "_" + y + ".png";
-
-                tiles[x][y] = new TETile(
-                        ' ',               // ASCII char (unused)
-                        Color.black,       // text color
-                        Color.black,       // background color
-                        "img " + x + "," + y,
-                        file               // tile image path
-                );
-            }
-        }
-
-        return tiles;
-    }
 }
--- proj5/src/demo/BoringWorldDemo.java
+++ proj5/src/demo/BoringWorldDemo.java
@@ -1,38 +1,40 @@
 package demo;
 
-import edu.princeton.cs.algs4.StdDraw;
-import tileengine.*;
+import tileengine.TERenderer;
+import tileengine.TETile;
+import tileengine.Tileset;
 
+/**
+ *  Draws a world that is mostly empty except for a small region.
+ */
 public class BoringWorldDemo {
 
     private static final int WIDTH = 60;
     private static final int HEIGHT = 30;
 
     public static void main(String[] args) {
-
-        // Expand canvas for HUD space
-
-
+        // initialize the tile rendering engine with a window of size WIDTH x HEIGHT
         TERenderer ter = new TERenderer();
         ter.initialize(WIDTH, HEIGHT);
 
-        // build world
+        // initialize tiles
         TETile[][] world = new TETile[WIDTH][HEIGHT];
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < HEIGHT; y++) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        // fills in a block 15 tiles wide by 5 tiles tall
+        for (int x = 20; x < 35; x++) {
+            for (int y = 5; y < 10; y++) {
                 world[x][y] = Tileset.WALL;
             }
         }
-        TETile[][] remTiles = loadImageTiles("resources/tiles", WIDTH, HEIGHT);
 
-        for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < tilesY; y++) {
-                world[startX + x][startY + y] = remTiles[x][y];
-            }
-        }
-
+        // draws the world to the screen
         ter.renderFrame(world);
-
-
     }
+
+
 }
