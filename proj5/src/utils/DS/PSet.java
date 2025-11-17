6a197e7ee4d8673e2f63313740fe6f52217ddda9
JudeAlexis111 <60825550+JudeAlexis111@users.noreply.github.com>
2025-11-16 21:31:18 -0800

    point, pset, tile, tset

---
+++ proj5/src/utils/DS/PSet.java
@@ -0,0 +1,51 @@
+package utils.DS;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+
+public class PSet extends HashSet<Point> {
+    public Point choose(Random random) {
+        int rIndex = random.nextInt(0, size() - 1);
+        Iterator<Point> iterator = iterator();
+
+        while(rIndex > 0) {
+            iterator.next();
+            rIndex--;
+        }
+
+        return iterator.next();
+    }
+
+    public PSet filterM(Point point, int radius, boolean in) {
+        PSet filtered = new PSet();
+
+        for (Point PSetPoints : this) {
+            if ((point.mDist(PSetPoints) <= radius) == in) {
+                filtered.add(PSetPoints);
+            }
+        }
+
+        return filtered;
+    }
+
+    public Point[] closestE(PSet uddaPSet) {
+        double minDist = Double.POSITIVE_INFINITY;
+        Point[] result = new Point[2];
+
+        for (Point point: this) {
+            for (Point uddaPoint: uddaPSet) {
+                double dist = point.eDist(uddaPoint);
+
+                if (minDist > dist) {
+                    minDist = dist;
+
+                    result[0] = point;
+                    result[1] = uddaPoint;
+                }
+            }
+        }
+
+        return result;
+    }
+}
